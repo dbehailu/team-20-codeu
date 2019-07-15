b4ecd8ddf83a05708830ca64c470c7cda062cad2
Dagm Behailu <dagmbehailu@gmail.com>
2019-07-14 19:37:05 -0700

    FormHandlerServletLogic

--- src/main/java/com/google/codeu/servlets/FormHandlerServlet.java
+++ src/main/java/com/google/codeu/servlets/FormHandlerServlet.java
@@ -4,6 +4,12 @@ import com.google.appengine.api.blobstore.*;
 import com.google.appengine.api.images.ImagesService;
 import com.google.appengine.api.images.ImagesServiceFactory;
 import com.google.appengine.api.images.ServingUrlOptions;
+import com.google.appengine.api.users.UserService;
+import com.google.appengine.api.users.UserServiceFactory;
+import com.google.codeu.data.Datastore;
+import com.google.codeu.data.Message;
+import org.jsoup.Jsoup;
+import org.jsoup.safety.Whitelist;
 
 import javax.servlet.ServletOutputStream;
 import javax.servlet.annotation.WebServlet;
@@ -17,24 +23,54 @@ import java.util.Map;
 @WebServlet("/my-form-handler")
 public class FormHandlerServlet extends HttpServlet {
 
+    private Datastore datastore;
+
+    @Override
+    public void init() {
+        datastore = new Datastore();
+    }
+
+    public String basicMarkdown(String text) {
+        /**
+         Regex converts anything matching ** [ANYTHING OTHER THAN *s] **
+         into <b> [ANYTHING OTHER THAN *s] </b>
+         */
+        text = text.replaceAll("\\*\\*([^\\*]*)\\*\\*", "<b>$1</b>");
+
+        /**
+         Regex for italicizing -- converts * [text] *
+         <i> [text] </i>
+         */
+        text = text.replaceAll("\\*([^\\*]*)\\*", "<i>$1</i>");
+
+        text = text.replaceAll("_([^\\_]*)_", "<u>$1</u>");
+        return text;
+    }
+
     @Override
     public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
 
-        // Get the message entered by the user.
-        String message = request.getParameter("message");
+        UserService userService = UserServiceFactory.getUserService();
+        if (!userService.isUserLoggedIn()) {
+            response.sendRedirect("/index.html");
+            return;
+        }
 
-        // Get the URL of the image that the user uploaded to Blobstore.
+        String user = userService.getCurrentUser().getEmail();
+        String userText = Jsoup.clean(request.getParameter("text"), Whitelist.relaxed());
+        String regex = "(https?://\\S+\\.(png|jpg|jpeg|gif))";
+        String replacement = "<img src=\"$1\" />";
+        String textWithImagesReplaced = userText.replaceAll(regex, replacement);
+        String textWithImagesReplacedMarkdown = basicMarkdown(textWithImagesReplaced);
         String imageUrl = getUploadedFileUrl(request, "image");
+        if (imageUrl != null) {
+            imageUrl = "<a href=\"" + imageUrl + "\">" + "<img src=\"" + imageUrl + "\">";
+            textWithImagesReplacedMarkdown=textWithImagesReplacedMarkdown+imageUrl;
+        }
+        Message message = new Message(user,textWithImagesReplacedMarkdown,imageUrl);
+        datastore.storeMessage(message);
 
-        // Output some HTML that shows the data the user entered.
-        // A real codebase would probably store these in Datastore.
-        ServletOutputStream out = response.getOutputStream();
-        out.println("<p>Here's the image you uploaded:</p>");
-        out.println("<a href=\"" + imageUrl + "\">");
-        out.println("<img src=\"" + imageUrl + "\" />");
-        out.println("</a>");
-        out.println("<p>Here's the text you entered:</p>");
-        out.println(message);
+        response.sendRedirect("/user-page.jsp?user=" + user);
     }
 
     /**
@@ -57,6 +93,7 @@ public class FormHandlerServlet extends HttpServlet {
         BlobInfo blobInfo = new BlobInfoFactory().loadBlobInfo(blobKey);
         if (blobInfo.getSize() == 0) {
             blobstoreService.delete(blobKey);
+            System.out.println("<p>the user did not select a file</p>");
             return null;
         }
 
