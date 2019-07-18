b3682aca45d6e9368ddb571754ae1926f15a8830
Sirinda Wongpanich <sirinda@berkeley.edu>
2019-07-17 21:58:00 -0700

    item page js added

--- src/main/webapp/MessageDetail.java
+++ src/main/webapp/MessageDetail.java
@@ -1,4 +1,13 @@
-package PACKAGE_NAME;
+import java.util.UUID;
 
 public class MessageDetail {
+    private UUID id;
+    private String user;
+    private String text;
+    private long timestamp;
+
+    private String title;
+    private String description;
+    private String location;
+    private String lostOrFound;
 }
